33f7f97d8009e658a50d0add7cf9e6d94b9650e7
Szymon Kowalski <>
2026-06-16 23:07:08 +0200

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
     >
       <div className="text-center">
         <h1 className="text-9xl font-bold text-black">404</h1>
-        <p className="mb-8 text-2xl font-medium text-black">Strona nie znaleziona</p>
+        <p className="mb-8 text-2xl font-medium text-black">Strona nie znaleziona!</p>
         <Button asChild className="px-8 py-4 text-lg">
           <Link href="/">Powrót</Link>
         </Button>
