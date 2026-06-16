cedb847ab8ba44c562cb333dd9292a08d5d08e6d
Szymon Kowalski <>
2026-06-16 23:23:51 +0200

    not found

--- app/api/offers/route.ts
+++ app/api/offers/route.ts
@@ -1,12 +1,8 @@
-
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
 export async function GET() {
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
   const { data, error } = await supabase.from('offers').select();
 
   if (error) return NextResponse.json({ error: error.message }, { status: 500 });
